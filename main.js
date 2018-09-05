82ae66bdc41f1338630930dec78c63db582ce48c
Ropah <ropah.musvaire@gmail.com>
2018-09-05 22:01:24 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas", ];
+var insertY = ["the soup kitchen", "Disneyland", "the White House"];
+var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+    var newStory = storyText;
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+    if (customName.value !== '') {
+        var name = customName.value;
+        newStory = newStory.replace("Bob", name);
+
+    }
+
+    if (document.getElementById("uk").checked) {
+        var weight = Math.round(poundsToStone(300)) + " stone";
+        newStory = newStory.replace("300 pounds", weight);
+        var temperature = Math.round(fahrenheitToCelcius(94)) + " centigrade";
+        newStory = newStory.replace("94 fahrenheit", temperature);
+    }
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+    customName.focus();
+}
+
+
+
+function fahrenheitToCelcius(temperature) {
+    return ((temperature - 32) / 1.8);
+}
+
+function poundsToStone(weight) {
+    return (weight / 14);
+}
+
+
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
